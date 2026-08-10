21e646d20aa76bd2cdbbeb2ecc6aa693ce763eb8
swape-commits <swapnilshah622000@gmail.com>
2026-08-10 09:12:46 -0400

    Add delete command with index validation

--- expense-tracker/tracker.py
+++ expense-tracker/tracker.py
@@ -9,6 +9,8 @@ add_parser.add_argument('--date',type=str,required=True)
 list_parser = subparsers.add_parser('list')
 list_parser.add_argument('--category',type=str,required=False)
 summary_parser =subparsers.add_parser('summary')
+delete_parser = subparsers.add_parser('delete')
+delete_parser.add_argument('--index',type=int,required=True)
 args = parser.parse_args()
 if args.command =='add':
     new_expense = {'amount':args.amount,'category':args.category,'date':args.date}
@@ -48,6 +50,20 @@ elif args.command =='summary':
 
     for month_year,total in monthly_totals.items():
         print(f"{month_year}: ${total:.2f}")
-        
+elif args.command =='delete':
+    try:
+        with open('expense.json','r') as f:
+            expenses =json.load(f)
+    except FileNotFoundError:
+        expenses = []
+    if args.index <0 or args.index >= len(expenses):
+                print("Invalid Index")
+    else:
+        removed = expenses.pop(args.index)
+        with open('expense.json','w') as f:
+            json.dump(expenses,f)
+        print(f"Deleted: ${removed['amount']:.2f} - {removed['category']} - {removed['date']}")
+ 
+
 
         
