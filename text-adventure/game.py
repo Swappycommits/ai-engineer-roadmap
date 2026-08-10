518d6262090911e468b7d7364a7b065a973a0b13
swape-commits <swapnilshah622000@gmail.com>
2026-08-10 10:41:19 -0400

    Player/room Inventory

--- text-adventure/game.py
+++ text-adventure/game.py
@@ -1,9 +1,10 @@
 class Room:
-    def __init__(self,description,exits):
+    def __init__(self,description,exits,items):
         self.description = description
         self.exits = exits
-forest = Room("You are in a quiet forest clearing.", {})
-cave = Room("You are in a dark cave.", {})
+        self.items = items
+forest = Room("You are in a quiet forest clearing.", {},[])
+cave = Room("You are in a dark cave.", {},['rusty key'])
 
 forest.exits = {"north": cave}
 cave.exits = {"south": forest}
@@ -11,12 +12,19 @@ cave.exits = {"south": forest}
 class Player:
     def __init__(self, current_room):
         self.current_room = current_room
+        self.inventory=[]
     def move(self,direction):
         if direction in self.current_room.exits:
             self.current_room= self.current_room.exits[direction]
         else:
             print("you cant go that way")
-        
+    def pick_up(self,item_name):
+        if item_name in self.current_room.items:
+            self.current_room.items.remove(item_name)
+            self.inventory.append(item_name)
+        else:
+            print(f"{item_name} is not in the room")
+       
         
 
 player =Player(forest)
@@ -25,3 +33,9 @@ player.move("north")
 print(player.current_room.description)
 
 player.move("north")
+
+player.pick_up("rusty key")
+print(player.inventory)
+print(cave.items)
+
+player.pick_up("sword")
