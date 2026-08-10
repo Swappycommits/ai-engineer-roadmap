b736e7707e8267f212a03f409ba392275b957487
swape-commits <swapnilshah622000@gmail.com>
2026-08-10 11:03:13 -0400

    Combat Feature added with damage and defeat detection

--- text-adventure/game.py
+++ text-adventure/game.py
@@ -13,6 +13,7 @@ class Player:
     def __init__(self, current_room):
         self.current_room = current_room
         self.inventory=[]
+        self.health = 100
     def move(self,direction):
         if direction in self.current_room.exits:
             self.current_room= self.current_room.exits[direction]
@@ -24,6 +25,25 @@ class Player:
             self.inventory.append(item_name)
         else:
             print(f"{item_name} is not in the room")
+    def attack(self,enemy):
+        enemy.health = enemy.health -10
+        print(f"You attack! Enemy health is now {enemy.health}")
+
+        if enemy.health<=0:
+            print("Enemy Killed! You Win ")
+
+        
+class Enemy:
+    def __init__(self,health,attack_power):
+        self.health = health
+        self.attack_power = attack_power
+    def attack(self,player):
+        player.health = player.health - self.attack_power
+        print(f"Enemy attacked! your health is {player.health}")
+
+        if player.health <=0:
+            print("You die! Game Over")
+
        
         
 
@@ -39,3 +59,13 @@ print(player.inventory)
 print(cave.items)
 
 player.pick_up("sword")
+
+goblin = Enemy(30, 5)
+player.attack(goblin)
+print(goblin.health)
+
+goblin.attack(player)
+print(player.health)
+
+player.attack(goblin)
+player.attack(goblin)
